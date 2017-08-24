07387cf3e47e87a15c8de3abe143a9515a8732cc
ccodam <clauscodam@gmail.com>
2017-08-24 09:55:25 +0200

    Add project files.

---
+++ SmartExplorer_SyncConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartExplorer_SyncConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
